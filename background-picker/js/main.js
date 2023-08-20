1fce26a21942690a0023e001d7dea24b32f9c8b5
June Monton <june.webdeveloper@gmail.com>
2023-08-20 18:25:22 +0800

    Added additional inputs for the youtube backgroundpicker exercise

--- background-picker/js/main.js
+++ background-picker/js/main.js
@@ -1,6 +1,13 @@
 document.getElementById('purple').onclick = partyPurple
 document.getElementById('green').onclick = partyGreen
 document.getElementById('blue').onclick = partyBlue
+document.getElementById('orange').onclick=partyOrange
+document.getElementById('yellow').onclick=partyYellow
+document.getElementById('pink').onclick=partyPink
+document.getElementById('navyblue').onclick=partyNavyBlue
+document.getElementById('red').onclick=partyRed
+document.getElementById('brown').onclick=partyBrown
+document.getElementById('beige').onclick=partyBeige
 
 
 function partyPurple() {
@@ -16,4 +23,39 @@ function partyGreen() {
 function partyBlue() {
   document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
   document.querySelector('body').style.color = 'white'
+}
+
+function partyOrange(){
+  document.querySelector('body').style.backgroundColor='rgba(252, 214, 112)'
+  document.querySelector('body').style.color='white'
+}
+
+function partyYellow(){
+  document.querySelector('body').style.backgroundColor='rgba(255, 252, 127)'
+  document.querySelector('body').style.color='white'
+}
+
+function partyPink(){
+  document.querySelector('body').style.backgroundColor='rgba(227, 61, 148)'
+  document.querySelector('body').style.color='white'
+}
+
+function partyNavyBlue(){
+  document.querySelector('body').style.backgroundColor='rgba(30, 81, 123)'
+  document.querySelector('body').style.color='white'
+}
+
+function partyRed(){
+  document.querySelector('body').style.backgroundColor='rgba(255, 76, 48)'
+  document.querySelector('body').style.color='white'
+}
+
+function partyBrown(){
+  document.querySelector('body').style.backgroundColor='rgba(166, 145, 92)'
+  document.querySelector('body').style.color='white'
+}
+
+function  partyBeige(){
+  document.querySelector('body').style.backgroundColor='rgba(227, 186, 143)'
+  document.querySelector('body').style.color='white'
 }
